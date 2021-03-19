61ae31592f71b4202d9b18bc9a46db6297156cbc
nicolas <a2018115260@estudiantes.upsa.edu.bo>
2021-03-19 17:03:32 -0400

    Se agrego base y DataContext

---
+++ 2prepparcialresuelto/2prepparcial/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _2prepparcial.Models
+{
+    public class Country
+    {
+            [Key]
+            public string Name { get; set; }
+            public string Capital { get; set; }
+            public int Population { get; set; }
+            public List<double> Lating { get; set; }
+            public List<string> Timezones { get; set; }
+            public string Flag { get; set; }
+    }
+}
